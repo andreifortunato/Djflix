ae25e81d9c2018ad2ae10fa991f3e918bb9ac860
Andrei Fortunato <54864576+andreifortunato@users.noreply.github.com>
2020-12-14 23:03:20 -0300

    Add files via upload

---
+++ src/components/Menu/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Logo  from '../../assets/img/Logo.png';
+import {Link} from 'react-router-dom';
+import './Menu.css';
+// import ButtonLink from '../components/ButtonLink';
+import Button from '../Button';
+
+
+function Menu() {
+    return (
+        <nav className='Menu'>
+            <Link  to='/'>
+            <img className="Logo" src={Logo}  alt=" DjFlix  logo"/>
+            </Link>
+
+            <Button as={Link} className='ButtonLink' to="cadastro/video"> 
+    
+             Novo video
+            </Button>
+
+        </nav>
+    )
+}
+
+export default Menu;
