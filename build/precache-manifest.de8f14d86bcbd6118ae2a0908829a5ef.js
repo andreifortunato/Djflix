062bbe0688e9f7a3edd3d0c4539d9f7d57fce5a5
Andrei Fortunato <54864576+andreifortunato@users.noreply.github.com>
2020-12-14 23:00:36 -0300

    Add files via upload

---
+++ build/precache-manifest.de8f14d86bcbd6118ae2a0908829a5ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439fdd6ad07dbb91745c0552d891cc48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1eb4f2d2174696c9ebe",
+    "url": "/static/css/main.a9f44cba.chunk.css"
+  },
+  {
+    "revision": "5df56c75438da1e3c5ee",
+    "url": "/static/js/2.2a3a86e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a3a86e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1eb4f2d2174696c9ebe",
+    "url": "/static/js/main.ad451737.chunk.js"
+  },
+  {
+    "revision": "25dc0492fee7feeb1d3d",
+    "url": "/static/js/runtime-main.f4376731.js"
+  },
+  {
+    "revision": "9d7e95fb1368ce9a2aba296f324d505b",
+    "url": "/static/media/Logo.9d7e95fb.png"
+  }
+]);
